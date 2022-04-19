6493507ac23ef19673fc669b6ffba6c3f0609463
Ella711 <allenro711@gmail.com>
2022-04-19 13:12:15 -0500

    [FIX] - provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Using Puppet, install puppet-lint.
 # Requirements:
-  # Install puppet-lint
-  # Version must be 2.5.0
+#     Install puppet-lint
+#     Version must be 2.5.0
 
 package { 'puppet-lint':
   ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org',
+  provider => gem,
+  source   => 'http://rubygems.org',
 }
