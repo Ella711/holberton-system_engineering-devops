2a825cc4b30b686739bc3384c7fdb52f414cfb6a
Ella711 <allenro711@gmail.com>
2022-05-04 18:23:54 -0500

    [ADD] - Files for task0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Script to fix failed requests on a Nginx server.
+
+exec { 'fix failed-requests':
+  command => "sed -i 's/15/20000/' /etc/default/nginx && sudo service nginx restart",
+  path    => '/bin',
+}
