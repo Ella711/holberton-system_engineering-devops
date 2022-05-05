27b28f3ca9b14e7eed6662f9e20b8cf0b1965339
Ella711 <allenro711@gmail.com>
2022-05-05 14:27:40 -0500

    [ADD] - File for task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Script to fix failed requests on a Nginx server.
+# Puppet script that fixes too many files opened error
 
-exec { 'fix ulimit':
-  command => 'sed -i "s/15/2000/g" /etc/default/nginx && sudo service nginx restart',
+exec {'fix ulimit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/bin',
 }
