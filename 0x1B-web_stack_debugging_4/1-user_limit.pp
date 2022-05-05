30636f14d6908f2a47081e7d02aaeca7202c2f64
Ella711 <allenro711@gmail.com>
2022-05-05 13:46:02 -0500

    [ADD] - File for task1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 # Bash script to change the OS configuration so that it is possible to login and open a file without any error message.
 
 exec { 'fix user-limit':
-  command => "sed -e 's/5/21000/g' -e 's/4/16000/g' /etc/security/limits.conf",
+  command => "sed -e 's/nofile 5/21000/g' -e 's/nofile 4/16000/g' /etc/security/limits.conf",
   path    => '/bin',
 }
 
