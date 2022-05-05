bd9554f8268f1cd2da7fbf1d842388dbfa84d661
Ella711 <allenro711@gmail.com>
2022-05-05 14:36:07 -0500

    [ADD] - File for task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # Puppet script that fixes too many files opened error
 
 exec {'fix ulimit':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/bin',
-}
-
-exec {'fix ulimits':
-  command => 'sed -i "4i worker_rlimit_nofile 30000;" /etc/nginx/nginx.conf && sudo service nginx restart',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
   path    => '/bin',
 }
