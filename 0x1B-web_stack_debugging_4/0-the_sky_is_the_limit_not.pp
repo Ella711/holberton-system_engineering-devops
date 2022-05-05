c47c95a940fde7bec95972278e108df87e33ccf1
Ella711 <allenro711@gmail.com>
2022-05-05 14:01:02 -0500

    [ADD] - File for task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,7 @@
 # Script to fix failed requests on a Nginx server.
 
 exec { 'fix failed-requests':
-  command => "sed -i 's/15/20000/' /etc/default/nginx",
-  path    => '/bin',
-}
-
-exec { 'fix failed':
-  command => "sed -i '4i worker_rlimit_nofile 30000;' /etc/nginx/nginx.conf && sudo service nginx restart",
+  command => "sed -i s/15/20000/ /etc/default/nginx",
   path    => '/bin',
 }
 
