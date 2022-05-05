fd4825dce0c7348f21e14c5de3e46b04e7fdfe11
Ella711 <allenro711@gmail.com>
2022-05-05 14:32:19 -0500

    [ADD] - File for task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,8 @@ exec {'fix ulimit':
   command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
   path    => '/bin',
 }
+
+exec {'fix ulimits':
+  command => 'sed -i "4i worker_rlimit_nofile 30000;" /etc/nginx/nginx.conf && sudo service nginx restart',
+  path    => '/bin',
+}
