5d869f71e25e031e6e8e4b2582a8c9cc92b423d0
Ella711 <allenro711@gmail.com>
2022-05-19 15:10:51 -0500

    [ADD] - File for task3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,13 +10,12 @@ def all_to_json():
     """
     Export the API data into json format
     """
-    api_url = "https://jsonplaceholder.typicode.com"
-    users = requests.get(api_url + "/users").json()
+    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
     user_list = []
     for user in users:
         user_list.append((user.get("id"), user.get("username")))
 
-    todo = requests.get(api_url + "/todos").json()
+    todo = requests.get("http://jsonplaceholder.typicode.com/todos").json()
     todo_list = []
     for tasks in todo:
         todo_list.append((tasks.get("userId"),
@@ -27,9 +26,10 @@ def all_to_json():
     for user in user_list:
         task_list = []
         for task in todo_list:
-            task_list.append({"username": user[1],
-                             "task": task[2],
-                              "completed": task[1]})
+            if task[0] == user[0]:
+                task_list.append({"username": user[1],
+                                  "task": task[2],
+                                  "completed": task[1]})
         all_dict[str(user[0])] = task_list
 
     filename = "todo_all_employees.json"
